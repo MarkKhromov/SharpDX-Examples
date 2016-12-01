a73023be8a7c8ba4e7942beb26937bd65cf8c6b9
Mark <lane.71rus@gmail.com>
2016-12-01 20:29:29 +0300

    Add SharpDX examples project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpDX Examples")]
+[assembly: AssemblyProduct("SharpDX Examples")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6a69b695-bb82-4915-b6a7-d203a64cb16e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
