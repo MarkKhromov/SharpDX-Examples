a9ab35e3a8d91d21dfa3fce2ffc0eb7bfafc5e82
Mark <lane.71rus@gmail.com>
2016-12-02 20:11:32 +0300

    Add texturing example

---
+++ Examples/Core/Helpers/TextureLoader.cs
@@ -0,0 +1,54 @@
+﻿using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.WIC;
+using Device = SharpDX.Direct3D11.Device;
+
+namespace SharpDXExamples.Examples.Core.Helpers {
+    public static class TextureLoader {
+        public static Texture2D LoadTexture2D(Device device, string fileName) {
+            var factory = new ImagingFactory2();
+            var bitmapSource = LoadBitmap(factory, fileName);
+            factory.Dispose();
+            int stride = bitmapSource.Size.Width * 4;
+            using(var buffer = new DataStream(bitmapSource.Size.Height * stride, true, true)) {
+                bitmapSource.CopyPixels(stride, buffer);
+                var texture = new Texture2D(device, new Texture2DDescription {
+                    Width = bitmapSource.Size.Width,
+                    Height = bitmapSource.Size.Height,
+                    ArraySize = 1,
+                    BindFlags = BindFlags.ShaderResource,
+                    Usage = ResourceUsage.Immutable,
+                    CpuAccessFlags = CpuAccessFlags.None,
+                    Format = Format.R8G8B8A8_UNorm,
+                    MipLevels = 1,
+                    OptionFlags = ResourceOptionFlags.None,
+                    SampleDescription = new SampleDescription(1, 0)
+                }, new DataRectangle(buffer.DataPointer, stride));
+                bitmapSource.Dispose();
+                return texture;
+            }
+        }
+
+        static BitmapSource LoadBitmap(ImagingFactory2 factory, string fileName) {
+            var bitmapDecoder = new BitmapDecoder(
+                factory,
+                fileName,
+                DecodeOptions.CacheOnDemand
+            );
+
+            var formatConverter = new FormatConverter(factory);
+
+            formatConverter.Initialize(
+                bitmapDecoder.GetFrame(0),
+                PixelFormat.Format32bppPRGBA,
+                BitmapDitherType.None,
+                null,
+                0.0,
+                BitmapPaletteType.Custom
+            );
+
+            return formatConverter;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using SharpDXExamples.Examples.BuffersShadersHLSL;
 using SharpDXExamples.Examples.Core;
 using SharpDXExamples.Examples.InitializingDirectX;
+using SharpDXExamples.Examples.Texturing;
 
 namespace SharpDXExamples {
     class Program {
         static void Main(string[] args) {
             Menu.RegisterItem(InitializingDirectX.Title, ExampleManager.Run<InitializingDirectX>);
             Menu.RegisterItem(BuffersShadersHLSL.Title, ExampleManager.Run<BuffersShadersHLSL>);
+            Menu.RegisterItem(Texturing.Title, ExampleManager.Run<Texturing>);
             Menu.Show();
         }
     }
